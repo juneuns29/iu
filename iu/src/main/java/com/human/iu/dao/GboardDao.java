bd696200333d7d9573d52c68906ae6489da810d1
juneuns29 <juneuns@gmail.com>
2024-04-02 16:13:46 +0900

    dao 빈처리 추가

---
+++ iu/src/main/java/com/human/iu/dao/GboardDao.java
@@ -0,0 +1,9 @@
+package com.human.iu.dao;
+
+public class GboardDao {
+
+	public GboardDao() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ iu/src/main/java/com/human/iu/dao/MemberDao.java
@@ -0,0 +1,9 @@
+package com.human.iu.dao;
+
+public class MemberDao {
+
+	public MemberDao() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
